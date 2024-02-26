96840f1d80b69542f365022a761a6e3314f43c35
Avatar <118656867+AvatarGujjar@users.noreply.github.com>
2024-02-26 15:18:30 +0530

    update CORS authorization

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ mongoDB();
 app.use(cors({
   origin: 'http://localhost:3000', // Allow requests from this origin
   methods: ["POST", "GET"], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow sending cookies with requests
 }));
 app.use(cors());
