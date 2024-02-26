b4a098d71d72e130123b1285a88be292af412ac2
Avatar <118656867+AvatarGujjar@users.noreply.github.com>
2024-02-26 17:00:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoDB();
 
 // Use the cors middleware
 app.use(cors({
-  origin: 'https://food-app-frontend-lake.vercel.app', // Allow requests from this origin
+  origin: "https://food-app-frontend-2.vercel.app", // Allow requests from this origin
   methods: ["POST", "GET"], // Allow these HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow sending cookies with requests
